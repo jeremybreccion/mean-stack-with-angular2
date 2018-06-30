f6f422f01542c9a5cda0a0b916673fd9a11b3533
jeremybreccion <34333326+jeremybreccion@users.noreply.github.com>
2018-06-30 17:13:38 +0800

    implemented reverse functionality

--- client/src/app/directives/validate-equal.directive.ts
+++ client/src/app/directives/validate-equal.directive.ts
@@ -1,7 +1,21 @@
 import { Directive, Input } from '@angular/core';
 import { Validator, AbstractControl, NG_VALIDATORS } from '@angular/forms'; 
 
-//how to use in an input field: [appValudateEqual] validateEqual="password" //name of the input control to be compared to
+/**
+ * How to use:
+ * e.g. passwords
+ * //legit password
+ * <input type="password" matInput placeholder="Password" [(ngModel)]="registerClass.password" name="password" #password="ngModel"
+          appValidateEqual validateEqual="confirmPassword" reverse="true" required>
+  //confirm password
+ * <input type="password" matInput placeholder="Confirm Password" [(ngModel)]="tempConfirmPassword" name="confirmPassword" #confirmPassword="ngModel"
+          appValidateEqual validateEqual="password" required>
+
+    //basically, use appValiadteEqual & validateEqual input on both values. use reverse input on the original or intended value
+ */
+
+
+
 @Directive({
   selector: '[appValidateEqual]',
   providers: [{
@@ -11,19 +25,45 @@ import { Validator, AbstractControl, NG_VALIDATORS } from '@angular/forms';
   }]
 })
 
-export class ValidateEqualDirective implements Validator{
+export class ValidateEqualDirective implements Validator {
 
   constructor() { }
 
   //directive input 
   @Input() validateEqual: string;
+  @Input() reverse: string;
 
-  validate(confirm: AbstractControl): { [key: string]: any } | null {
+  private isReverse() {
+    return (this.reverse === 'true') ? true : false;
+  }
 
-    //get the original input (e.g. password)
-    let original = confirm.root.get(this.validateEqual);
+  validate(self: AbstractControl): { [key: string]: any } | null {
 
-    return (original && confirm.value !== original.value) ? {equal: true} : null;
+    //get the compareTo input (e.g. password)
+    let compareTo = self.root.get(this.validateEqual);
+
+    //input mismatch
+    if(compareTo && self.value !== compareTo.value && !this.isReverse()) {
+      //send error equal set to true
+      return { equal: true };
+    }
+
+    //input match
+    if(compareTo && self.value === compareTo.value && this.isReverse()) {
+      //delete errors
+      delete self.errors['equal'];
+      //
+      if(!Object.keys(self.errors).length) {
+        self.setErrors(null);
+      }
+    }
+
+    //input mismatch
+    if(compareTo && self.value !== compareTo.value && this.isReverse()) {
+      compareTo.setErrors({ equal: true });
+    }
+
+    return null;
   }
 
 }
