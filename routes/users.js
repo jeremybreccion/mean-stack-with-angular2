ede62bfafa8a0fc8c80839e6f9675282da6bbb9b
jeremybreccion <34333326+jeremybreccion@users.noreply.github.com>
2018-06-30 19:07:45 +0800

    minor

--- routes/users.js
+++ routes/users.js
@@ -10,16 +10,15 @@ const bcrypt = require('bcryptjs');
 
 
 router.post('/register', (req, res) => {
-    console.log(req.body);
     //hash the password
     req.body.password = bcrypt.hashSync(req.body.password);
-    console.log(req.body);
 
-    var newUser = models.User(req.body);
+    const newUser = models.User(req.body);
 
     newUser.save((err) => {
         if(err) {
-            res.status(400).send();
+            console.log(err);
+            res.status(400).send(err);
         }
 
         res.status(200).send();
