9f0f58b601bd0151d1db28e61ecf2cb2c7d85537
jeremybreccion <34333326+jeremybreccion@users.noreply.github.com>
2018-06-30 17:21:31 +0800

    created httpclient service

---
+++ client/src/app/services/http.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+export class RegisterDetails {
+  email: string;
+  nickname: string;
+  password: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+
+  private handleError(error: HttpErrorResponse) {
+    if(error.error instanceof ErrorEvent) {
+      //client side/network error occurred
+      console.log('client side error occured');
+    } else {
+      console.log('error from server: ' + error.status + '\n' + error.error);
+    }
+
+    return throwError('Error occurred');
+  }
+
+  constructor(private http: HttpClient) { }
+
+  //if ng serve is used, you must declare the node server address
+  domain = 'http://localhost:8080';
+
+  /* httpOptions = {
+    headers: new HttpHeaders({
+      'Access-Control'
+    })
+  } */
+
+
+  register (registerDetails: RegisterDetails): Observable<RegisterDetails> {
+    console.log(registerDetails);
+    return this.http.post<RegisterDetails>(this.domain + '/users/register', registerDetails)
+    .pipe(catchError(this.handleError));
+  }
+}
