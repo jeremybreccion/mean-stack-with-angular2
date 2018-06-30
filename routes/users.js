b08932196ffef27116e5a54bf1b37b4a3c1c681e
jeremybreccion <34333326+jeremybreccion@users.noreply.github.com>
2018-06-30 18:24:54 +0800

    installed mongoose

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,12 @@
 
 const express = require('express');
 const router = express.Router();
-const mongoskin = require('mongoskin');
 const url = require('../config/database.json');
-const db = mongoskin.db(url.database, { useNewUrlParser: true });
+const mongoose = require('mongoose');
+mongoose.connect(url.database);
+const models = require('../models/models');
 const bcrypt = require('bcryptjs');
-db.bind('users');
+
 
 
 router.post('/register', (req, res) => {
@@ -13,13 +14,15 @@ router.post('/register', (req, res) => {
     //hash the password
     req.body.password = bcrypt.hashSync(req.body.password);
     console.log(req.body);
-    db.users.insert(req.body, (err) => {
+
+    var newUser = models.User(req.body);
+
+    newUser.save((err) => {
         if(err) {
-            console.log(err);
-            res.status(400).send(err);
-        } else {
-            res.status(200).send();
+            res.status(400).send();
         }
+
+        res.status(200).send();
     });
 
 });
