c1ee929d250c653afaaf4a4fc17c08c56d371815
jeremybreccion <34333326+jeremybreccion@users.noreply.github.com>
2018-07-28 10:16:27 +0800

    implemented express session

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
+const session = require('express-session');
 
 const port = 8080;
 const index = require('./routes/index');
 const users = require('./routes/users');
+const config = require('./config/config.json');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({secret: config.session_secret, resave: false, saveUninitialized: true}));
 
 //put route declarations here
 app.use('/', index);
