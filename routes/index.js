364fbec0dfc385a23329207dc9ede4ef951a70fb
jeremybreccion <34333326+jeremybreccion@users.noreply.github.com>
2018-07-28 10:18:24 +0800

    implemented get token middleware

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 
-router.get('/', (req, res, next) => {
+router.use('/', (req, res, next) => {
+    //console.log(req.path);
+    //ignore .js & .ico paths
+    if(req.path.substring('.js') === -1 && req.path.substring('.ico') === -1 && req.path !== '/users/login' && req.path !== '/users/register' && req.path !== '/users/current') {
+        //get auth header
+        const token = req.get('Authorization');
+        console.log(token);
+    }
     next();
 });
 
