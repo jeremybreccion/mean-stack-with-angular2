24d6da5869e3ce7f7e57e33e16be3404592ec940
jeremybreccion <34333326+jeremybreccion@users.noreply.github.com>
2018-07-31 21:00:03 +0800

    implemented check session during first open

--- client/src/app/services/auth-guard.service.ts
+++ client/src/app/services/auth-guard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot, CanActivateChild } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Injectable({
@@ -9,12 +9,32 @@ export class AuthGuardService implements CanActivate {
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     let url: string = state.url;
+
+    /**
+     * when user is logged on, he/she can still access login & register wews
+     * code below does not work
+     */
+
+    if(this.authService.isLoggedIn === undefined) {
+      if(url === '/login' || url === '/register') {
+        return true;
+      } else {
+        this.authService.current().subscribe((res: any) => {
+          this.authService.setToken(res.token);
+          this.router.navigateByUrl(url);
+          return true;
+        }, err => {
+          this.router.navigateByUrl('/login');
+          return false;
+        });
+      }
+    }
     
     if(this.authService.isLoggedIn) {
       
       this.authService.redirectUrl = url;
 
-      return true;
+      return (url !== '/login' && url !== '/register') ? true : false;
     }
 
     this.router.navigateByUrl('/login');
